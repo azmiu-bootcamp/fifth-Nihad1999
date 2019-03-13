7fa348720a54fa76813032352ebabf9327486b15
Nihad1999 <47653259+Nihad1999@users.noreply.github.com>
2019-03-13 15:29:39 +0300

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,17 @@
+
+
+def love6(a,b):
+    if a==6 or b==6:
+        print("True")
+    elif a+b==6 or a-b==6:
+        print("True")
+    else:
+        print("False")
+
+
+
+
+a = int(input("a-ni daxil edin-> "))
+b = int(input("b-ni daxil edin-> "))
+
+love6(a,b)
